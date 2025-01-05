2ab6556e61087da24396a7d8bb7ed11db1604da2
MrWooltrest <wooltrest@gmail.com>
2025-01-05 19:25:56 +0200

    Revert "fs: fixes for redefinition KernelSU"
    This reverts commit 94b87b1a4cc77459360340ed880a0359219afe02.

--- fs/stat.c
+++ fs/stat.c
@@ -96,7 +96,7 @@ extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *fla
 #endif
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
-		int flags)
+		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
@@ -107,13 +107,13 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		ksu_handle_stat(&dfd, &filename, &flags);
 #endif
 
-	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
+	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
 		goto out;
 
-	if (!(flags & AT_SYMLINK_NOFOLLOW))
+	if (!(flag & AT_SYMLINK_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
-	if (flags & AT_EMPTY_PATH)
+	if (flag & AT_EMPTY_PATH)
 		lookup_flags |= LOOKUP_EMPTY;
 retry:
 	error = user_path_at(dfd, filename, lookup_flags, &path);
